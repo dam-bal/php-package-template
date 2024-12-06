02d89565f689ce447b8c235f70af1529202a25ae
dam-bal <d.balandowski@icloud.com>
2024-12-06 12:04:01 +0100

    upgraded PHP version to latest (PHP 8.4), some changes to checks.sh

--- checks.sh
+++ checks.sh
@@ -1,9 +1,38 @@
-docker compose run --rm phpunit
+#!/usr/bin/env sh
 
-docker compose run --rm phpcs
+run_command() {
+    temp_file=$(mktemp)
+    $1 >"$temp_file" 2>&1
 
-docker compose run --rm phpstan
+    if [ $? -eq 0 ]; then
+        echo "\e[32mSUCCESS\e[0m"
+        rm "$temp_file"
+    else
+        echo "\e[31mFAIL\e[0m"
+        cat "$temp_file"
+        rm "$temp_file"
+    fi
+}
 
-docker compose run --rm phpmd
+echo "#########"
+echo "# Tests #"
+echo "#########"
+run_command "./vendor/bin/phpunit"
 
-docker compose run --rm phplint
+echo ""
+echo "###################"
+echo "# Static Analysis #"
+echo "###################"
+run_command "./vendor/bin/phpstan analyse src --level 9"
+
+echo ""
+echo "##################"
+echo "# Mess Detection #"
+echo "##################"
+run_command "./vendor/bin/phpmd src/ text codesize,unusedcode,design,cleancode"
+
+echo ""
+echo "###################"
+echo "# Code Formatting #"
+echo "###################"
+run_command "./vendor/bin/phpcs --standard=PSR12 src/"
